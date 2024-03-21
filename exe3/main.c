58697312f30eb07fea979db69b2a991f7748a7ce
ilacftemp <ilachafin@gmail.com>
2024-03-21 17:21:31 +0000

    ex3 pronto

--- exe3/main.c
+++ exe3/main.c
@@ -5,6 +5,7 @@
 
 #include "pico/stdlib.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "data.h"
 QueueHandle_t xQueueData;
@@ -25,13 +26,30 @@ void data_task(void *p) {
 
 void process_task(void *p) {
     int data = 0;
+    int janela[5];
+    int cont = 0;
+    int y = 0;
 
     while (true) {
         if (xQueueReceive(xQueueData, &data, 100)) {
+            // printf("%d\n", data);
             // implementar filtro aqui!
-
-
-
+            if(cont < 5){
+                janela[cont] = data;
+                cont++;
+            } else {
+                for(int i = 1; i < 5; i++){
+                    janela[i-1] = janela[i];
+                    y += janela[i-1]/5;
+                }
+                janela[4] = data;
+                y += janela[4]/5;
+                if((abs(data-y) > 5)){
+                    data = y;
+                }
+            }
+            y = 0;
+            printf("%d\n", data);
 
             // deixar esse delay!
             vTaskDelay(pdMS_TO_TICKS(50));
